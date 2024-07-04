c57a64f5fcdee4e408d5ce6de19beaefd72b1a35
Agustinsn <agustinasn95@gmail.com>
2024-07-04 01:31:20 -0500

    Avance: * Test Axios

--- src/app/login/services/testAxios.ts
+++ src/app/login/services/testAxios.ts
@@ -1,10 +1,9 @@
 import axios from 'axios';
-import { headers } from 'next/headers';
 
 export const testAxios = async () => {
     const response = await axios.post('https://www.google.com/recaptcha/api/siteverify',
         {
-            params:{secret:'6LcZ7QcqAAAAAPuMXSWEMmRtnc4UZbc5Pw6raHLy',response:'adasdasdasd'},
+            params:{secret:'',response:''},
             headers:{
                 'Content-Type':'application/x-www-form-urlencoded',
                 'Access-Control-Allow-Origin': '*',
