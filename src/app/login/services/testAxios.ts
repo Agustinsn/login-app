3d3795c5061f49e66831cbd0f1729d75ef19b94e
Agustinsn <agustinasn95@gmail.com>
2024-07-04 00:25:18 -0500

    Avance: * Test Axios

--- src/app/login/services/testAxios.ts
+++ src/app/login/services/testAxios.ts
@@ -1,8 +1,15 @@
 import axios from 'axios';
+import { headers } from 'next/headers';
 
 export const testAxios = async () => {
     const response = await axios.post('https://www.google.com/recaptcha/api/siteverify',
-        {params:{secret:'6LcZ7QcqAAAAABUvP6GKUKY3bK-K6gnCyeN2bVci',response:'adasdasdasd'}}
+        {
+            params:{secret:'6LcZ7QcqAAAAAPuMXSWEMmRtnc4UZbc5Pw6raHLy',response:'adasdasdasd'},
+            headers:{
+                'Content-Type':'application/x-www-form-urlencoded',
+                'Access-Control-Allow-Origin':'*'
+            }
+        }
     )
     console.log(response.data);
 }
